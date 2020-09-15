c153e4cbab57b6172702d0713602e59a56110e39
SoyOscarRH <soyoscarrh@gmail.com>
2020-09-14 21:27:48 -0500

    Better dashboard buttons

--- src/screens/Main/index.tsx
+++ src/screens/Main/index.tsx
@@ -21,7 +21,7 @@ const Main = () => {
             Schedule a Mock Interview
           </Box>
           <Box background="brand" className={styles.bigSectionButton}>
-            ...
+            Check my interviews history
           </Box>
           <Box background="brand" className={styles.bigSectionButton}>
             ...
