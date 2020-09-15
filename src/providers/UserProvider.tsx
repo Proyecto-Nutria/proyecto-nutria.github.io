cc95014ff25bdcabdb160cc60ad1c8aa7ca9346c
SoyOscarRH <soyoscarrh@gmail.com>
2020-09-15 00:45:13 -0500

    Update types in UserProvider

--- src/providers/UserProvider.tsx
+++ src/providers/UserProvider.tsx
@@ -1,16 +1,18 @@
 import React, { useState, createContext, useEffect } from "react";
 import { auth } from "./../firebase";
 
-const UserContext = createContext<firebase.User | null>(null);
+type maybeUser = firebase.User | null;
+const UserContext = createContext<maybeUser>(null);
 
+const Provider = UserContext.Provider;
 const UserProvider: React.FunctionComponent = ({ children }) => {
-  const [user, changeUser] = useState<firebase.User | null>(null);
+  const [user, changeUser] = useState<maybeUser>(null);
 
   useEffect(() => {
     auth.onAuthStateChanged((userAuth) => changeUser(userAuth));
   }, []);
 
-  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+  return <Provider value={user}>{children}</Provider>;
 };
 
 export { UserContext };
