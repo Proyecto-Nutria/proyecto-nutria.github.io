1090a9a29a463daad7d099500956fac587f6f68a
SoyOscarRH <soyoscarrh@gmail.com>
2020-09-15 21:30:16 -0500

    Cool

--- src/screens/Schedule/index.tsx
+++ src/screens/Schedule/index.tsx
@@ -1,6 +1,20 @@
 import React from "react"
+import { useQuery, gql } from "@apollo/client"
+
+const NAMES = gql`
+  query persons {
+    name
+  }
+`
 
 const Schedule = () => {
+  const { loading, error, data } = useQuery(NAMES)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
+  console.log(data)
+
   return <>hello</>
 }
 
