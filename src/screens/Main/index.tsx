30b80ce8092dec0c6e303cfbbccea8e7c18a22c0
akotadi <manuel_7795@hotmail.com>
2020-09-19 13:28:08 -0500

    Added buttons in dashboard for redirection

--- src/screens/Main/index.tsx
+++ src/screens/Main/index.tsx
@@ -18,15 +18,33 @@ const Main = () => {
         columns={["1fr", "1fr", "1fr"]}
         gap="medium"
       >
+      <Box
+        background="brand"
+        className={styles.bigSectionButton}
+        onClick={() => history.push("/scheduleInterviewee")}
+      >
+        Schedule a Mock Interview (Interviewee)
+      </Box>
         <Box
           background="brand"
           className={styles.bigSectionButton}
-          onClick={() => history.push("/schedule")}
+          onClick={() => history.push("/scheduleInterviewer")}
         >
-          Schedule a Mock Interview
+          Schedule a Mock Interview (Interviewer)
         </Box>
-        <Box background="brand" className={styles.bigSectionButton}>
-          Check my interviews history
+        <Box
+          background="brand"
+          className={styles.bigSectionButton}
+          onClick={() => history.push("/interviewsInterviewee")}
+        >
+          Check my interviews history (Interviewee)
+        </Box>
+        <Box
+          background="brand"
+          className={styles.bigSectionButton}
+          onClick={() => history.push("/interviewsInterviewer")}
+        >
+          Check my interviews history (Interviewer)
         </Box>
         <Box background="brand" className={styles.bigSectionButton}>
           Endpoint
