4839c13ebdf5fe21f4e886df94370198f62ec75d
searleser97 <gabrielsanv97@gmail.com>
2020-10-25 20:31:48 -0600

    test github workflow

---
+++ build/precache-manifest.a9d175c27a02cc5004c9577c75a1c588.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd6b7ce9a15119ee9857e1b272b07684",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68bf60bf1a8e945d9d10",
+    "url": "/static/css/main.b6eed0d3.chunk.css"
+  },
+  {
+    "revision": "6e772e9ecafc1d44860a",
+    "url": "/static/js/2.bc053971.chunk.js"
+  },
+  {
+    "revision": "68474a25a781230a9a35ecc384abd146",
+    "url": "/static/js/2.bc053971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68bf60bf1a8e945d9d10",
+    "url": "/static/js/main.28044ae4.chunk.js"
+  },
+  {
+    "revision": "ab9a29fc670bc8e08b0c",
+    "url": "/static/js/runtime-main.9b04fbb8.js"
+  }
+]);
