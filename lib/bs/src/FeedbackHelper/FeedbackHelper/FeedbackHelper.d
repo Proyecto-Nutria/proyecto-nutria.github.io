d0f95078ea77e75b26e2ee27fdf85c136cea41e8
¨Gober¨ <reyesfragosoroberto@gmail.com>
2020-10-26 19:15:09 -0600

    Ignore all libs folders

--- lib/bs/src/FeedbackHelper/FeedbackHelper/FeedbackHelper.d
+++
@@ -1,1 +0,0 @@
-src/FeedbackHelper/FeedbackHelper/FeedbackHelper-Yaos.cmj : Yaos.cmi src/FeedbackHelper/TextArea/TextArea-Yaos.cmj src/FeedbackHelper/TextArea/TextArea-Yaos.cmi src/FeedbackHelper/Modules/Time-Yaos.cmj src/FeedbackHelper/Modules/Time-Yaos.cmi src/FeedbackHelper/TimerView/TimerView-Yaos.cmj src/FeedbackHelper/TimerView/TimerView-Yaos.cmi
