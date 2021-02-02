75e6263f96362551ca7b3aeaa59c9ab7ac492815
Hugo Jimenez <hugoduar1@gmail.com>
2021-02-02 03:54:40 -0600

    Replace grommet header for material UI

--- src/components/Visitor/Landing/LandingHeader.tsx
+++ src/components/Visitor/Landing/LandingHeader.tsx
@@ -1,14 +1,32 @@
 import React from "react"
-import { Header, Anchor, Nav } from "grommet"
+import { AppBar, Toolbar, Typography, Button} from "@material-ui/core"
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 
-const VisitorHeader = () => (
-  <Header background="light-1" pad="medium">
-    <Anchor href="#" label="Nutria" />
-    <Nav direction="row">
-      <Anchor href="#" label="About Us" />
-      <Anchor href="#" label="Contributors" />
-    </Nav>
-  </Header>
-)
+const useStyles = makeStyles((theme: Theme) => 
+createStyles({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
 
-export default VisitorHeader
+export default function VisitorHeader() {
+  const classes = useStyles()
+
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <Typography variant="h6" className={classes.title}>
+          Nutria
+        </Typography>
+        <Button color="inherit">About</Button>
+        <Button color="inherit">Contributors</Button>
+      </Toolbar>
+    </AppBar>
+  )
+}
