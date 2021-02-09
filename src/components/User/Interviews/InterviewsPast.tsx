b54405a46aeb8bfca4f8734c7606dbcde6b3b995
¨Gober¨ <reyesfragosoroberto@gmail.com>
2021-02-09 11:54:23 -0600

    Replace past interviews with MaterialUI

--- src/components/User/Interviews/InterviewsPast.tsx
+++ src/components/User/Interviews/InterviewsPast.tsx
@@ -1,43 +1,41 @@
-import React from "react"
-import { Heading, Box, Text, DataTable, Anchor } from "grommet"
+import React from 'react';
+
+import Button from '@material-ui/core/Button';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 const InterviewsPast = (props: any) => {
   return (
-    <Box pad="xlarge">
-      <Heading level="3">Past Interviews</Heading>
-      <Box
-        round
-        pad="large"
-        align="center"
-        background="main-box"
-        elevation="large"
-        alignSelf="center"
-        flex={{ shrink: 0 }}
-      >
-        <DataTable
-          size="medium"
-          data={props.data}
-          sort={props.sort}
-          onSort={props.onSort}
-          primaryKey={false}
-          columns={[
-            {
-              property: "date",
-              header: <Text>Date</Text>,
-            },
-            {
-              property: "document",
-              header: <Text>Document</Text>,
-              sortable: false,
-              render: ({ document }) => (
-                <Anchor color="brand" href={document} label="Link" />
-              ),
-            },
-          ]}
-        />
-      </Box>
-    </Box>
-  )
-}
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">Date</TableCell>
+            <TableCell align="right">Document</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.data.map((row: any) => (
+            <TableRow key={row.name}>
+              <TableCell>{row.date}</TableCell>
+              <TableCell align="right">
+                <a href={row.document}>
+                  <Button variant="contained" color="primary">
+                    Document
+                  </Button>
+                </a>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
 
-export default InterviewsPast
+export default InterviewsPast;
