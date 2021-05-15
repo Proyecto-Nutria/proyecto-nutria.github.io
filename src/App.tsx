41a16549a807c8322c38529e5955456cf8408f57
¨Gober¨ <reyesfragosoroberto@gmail.com>
2021-05-15 00:03:26 -0500

    Replace old theme to material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
-
-import { Grommet, ThemeType } from 'grommet';
-import yaosTheme from 'assets/themes/yaos';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { purple } from '@material-ui/core/colors';
 
 import AuthProvider from 'providers/AuthProvider';
 import GraphProvider from 'providers/GraphProvider';
 import Routes from 'routes/Routes';
 
+// TODO: Migrate the theme to a proper file
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: purple[500],
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#11cb5f',
+    },
+  },
+});
+
 const App: React.FunctionComponent = () => {
   return (
-    <Grommet theme={yaosTheme as ThemeType}>
+    <ThemeProvider theme={theme}>
       <AuthProvider>
         <GraphProvider>
           <Routes />
         </GraphProvider>
       </AuthProvider>
-    </Grommet>
+    </ThemeProvider>
   );
 };
 
