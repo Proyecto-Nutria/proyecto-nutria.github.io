84b71fd3fb007543e2d2c50c4008efb7140770d0
hugoduar <hugoduar1@gmail.com>
2021-05-28 19:17:06 -0500

    Change width of schedule picker (#33)
    * Change width from schedule picker
    * Remove unused vars

--- src/components/Interviewee/Schedule/IntervieweeSchedule.tsx
+++ src/components/Interviewee/Schedule/IntervieweeSchedule.tsx
@@ -15,6 +15,7 @@ import Select from '@material-ui/core/Select';
 import Typography from '@material-ui/core/Typography';
 import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
+import { Grid } from '@material-ui/core';
 
 const IntervieweeSchedule = (props: any) => {
   const mutationFunction = props.mutation;
@@ -80,7 +81,7 @@ const IntervieweeSchedule = (props: any) => {
           >
             Schedule preference
           </Button>
-
+          <br />
           {Object.entries(props.dynamicInput.state).map(([id, data]) => {
             const current = props.dynamicInput.state[id];
             const { day, interval } = current;
@@ -88,27 +89,31 @@ const IntervieweeSchedule = (props: any) => {
             const updater = props.dynamicInput.setter;
             return (
               <div key={id}>
-                <FormControl>
-                  <InputLabel>Day</InputLabel>
-                  <Select
-                    value={day}
-                    onChange={event =>
-                      updater({
-                        type: 'updateDay',
-                        id,
-                        day: event.target.value,
-                      })
-                    }
-                  >
-                    {props.dynamicInput.values.days.map((day: any, id: any) => (
-                      <MenuItem key={id} value={day}>
-                        {day}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-
-                <FormControl>
+              <br />
+              <Grid container>
+                <Grid item xs>
+                  <FormControl fullWidth={true}>
+                    <InputLabel>Day</InputLabel>
+                    <Select
+                      value={day}
+                      onChange={event =>
+                        updater({
+                          type: 'updateDay',
+                          id,
+                          day: event.target.value,
+                        })
+                      }
+                    >
+                      {props.dynamicInput.values.days.map((day: any, id: any) => (
+                        <MenuItem key={id} value={day}>
+                          {day}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Grid item xs>        
+                <FormControl fullWidth={true}>
                   <InputLabel>Start</InputLabel>
                   <Select
                     value={start}
@@ -129,8 +134,9 @@ const IntervieweeSchedule = (props: any) => {
                     )}
                   </Select>
                 </FormControl>
-
-                <FormControl>
+                </Grid>
+                <Grid item xs>
+                <FormControl fullWidth={true}>
                   <InputLabel>End</InputLabel>
                   <Select
                     value={end}
@@ -151,17 +157,20 @@ const IntervieweeSchedule = (props: any) => {
                     )}
                   </Select>
                 </FormControl>
-
+                </Grid>        
                 <IconButton onClick={() => updater({ type: 'delete', id })}>
                   <DeleteIcon />
                 </IconButton>
+                </Grid>
               </div>
             );
           })}
-
-          <Button type="submit" variant="contained" color="primary">
-            Schedule
-          </Button>
+          <br/>
+          <Box>
+            <Button type="submit" variant="contained" color="primary">
+              Schedule
+            </Button>
+          </Box>
         </form>
         {props.onMutationError && <p>Error :( Please try again</p>}
       </Box>
