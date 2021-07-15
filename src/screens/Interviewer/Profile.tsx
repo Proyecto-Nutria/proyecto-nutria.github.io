4f9bf170115e6113a807df7be5be399e922e9153
¨Gober¨ <reyesfragosoroberto@gmail.com>
2021-07-15 16:26:03 -0500

    Put the correct format to push a new interviewer

--- src/screens/Interviewer/Profile.tsx
+++ src/screens/Interviewer/Profile.tsx
@@ -2,12 +2,15 @@ import InterviewerProfileForm from 'components/Interviewer/Profile/InterviewerPr
 import UserError from 'components/User/UserError';
 import { useIsFirstLogin } from 'hooks/UserHooks';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { LANDING_PATH } from 'routes/paths';
 import { INTERVIEWER_PROFILE_COPY } from 'utils/constants/copy';
 import { CREATE_INTERVIEWER, UPDATE_INTERVIEWER } from 'utils/constants/endpoints';
 
 import { useMutation } from '@apollo/client';
 
 const InterviewerEditProfile = () => {
+  let history = useHistory();
   const newUser = useIsFirstLogin();
   let profileMutation = CREATE_INTERVIEWER;
   if (!newUser) profileMutation = UPDATE_INTERVIEWER;
@@ -21,12 +24,25 @@ const InterviewerEditProfile = () => {
   if (modifyInterviewerMutationError) return <UserError />;
 
   const editInterviewer = () => {
-    modifyInterviewer({
-      variables: {
-        mentioned: appear,
-        information: about,
-      },
-    });
+    if (newUser) {
+      modifyInterviewer({
+        variables: {
+          information: {
+            mentioned: appear,
+            information: about,
+          },
+        },
+      });
+    } else {
+      modifyInterviewer({
+        variables: {
+          mentioned: appear,
+          information: about,
+        },
+      });
+    }
+    //TODO: Bug: Cannot redirect after first time
+    history.push(LANDING_PATH);
   };
 
   return (
