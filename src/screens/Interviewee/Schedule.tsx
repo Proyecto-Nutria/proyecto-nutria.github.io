ad175d3be82a959bb7edac25dd811c7e1974f2c1
¨Gober¨ <reyesfragosoroberto@gmail.com>
2021-08-14 15:25:16 -0500

    Add onLoad to pool to prevent double mutations

--- src/screens/Interviewee/Schedule.tsx
+++ src/screens/Interviewee/Schedule.tsx
@@ -1,5 +1,6 @@
 import IntervieweeSchedule from 'components/Interviewee/Schedule/IntervieweeSchedule';
 import UserError from 'components/User/UserError';
+import UserLoading from 'components/User/UserLoading';
 import React, { useReducer, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { INTERVIEWEE_SCHEDULE_COPY } from 'utils/constants/copy';
@@ -69,8 +70,10 @@ const avaliabilityReducer = (
 const IntervieweeMock = () => {
   let history = useHistory();
 
-  const [enterToPool, { error: enterToPoolMutationError }] =
-    useMutation(ENTER_POOL);
+  const [
+    enterToPool,
+    { loading: enterPoolLoading, error: enterToPoolMutationError },
+  ] = useMutation(ENTER_POOL);
 
   const [interviewType, setInterviewTypeValue] = useState('');
   const [rol, setRolValue] = useState('');
@@ -82,6 +85,7 @@ const IntervieweeMock = () => {
     {}
   );
 
+  if (enterPoolLoading) return <UserLoading />;
   if (enterToPoolMutationError) return <UserError />;
 
   const positionField: staticField = {
