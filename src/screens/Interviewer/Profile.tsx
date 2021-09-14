9d9142913b0696ea7c253cfd3a8074476d582fb6
searleser97 <gabrielsanv97@gmail.com>
2021-09-14 15:31:49 -0500

    Fix interviewer profile

--- src/screens/Interviewer/Profile.tsx
+++ src/screens/Interviewer/Profile.tsx
@@ -1,7 +1,6 @@
 import InterviewerProfileForm from 'components/Interviewer/Profile/InterviewerProfileForm';
 import UserError from 'components/User/UserError';
-import { useIsFirstLogin } from 'hooks/UserHooks';
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { HOME_PATH } from 'routes/paths';
 import { INTERVIEWER_PROFILE_COPY } from 'utils/constants/copy';
@@ -11,23 +10,41 @@ import {
 } from 'utils/constants/endpoints';
 
 import { useMutation } from '@apollo/client';
+import { UserStatusContext } from 'providers/UserStatusProvider';
 
 const InterviewerEditProfile = () => {
   let history = useHistory();
-  const newUser = useIsFirstLogin();
+  const { isNewUser, setIsNewUser } = useContext(UserStatusContext);
+  const [ableToRedirect, setAbleToRedirect] = React.useState(true);
   let profileMutation = CREATE_INTERVIEWER;
-  if (!newUser) profileMutation = UPDATE_INTERVIEWER;
+  if (!isNewUser) profileMutation = UPDATE_INTERVIEWER;
 
-  const [modifyInterviewer, { error: modifyInterviewerMutationError }] =
-    useMutation(profileMutation);
+  const [
+    modifyInterviewer,
+    { error: modifyInterviewerMutationError, data: responseStatus },
+  ] = useMutation(profileMutation);
 
   const [appear, setAppearValue] = React.useState(false);
   const [about, setAboutValue] = React.useState('');
 
+  useEffect(() => {
+    if (responseStatus && ableToRedirect) {
+      setIsNewUser(false);
+      setAbleToRedirect(false);
+      history.push(HOME_PATH);
+    }
+  }, [
+    ableToRedirect,
+    history,
+    responseStatus,
+    setIsNewUser,
+    setAbleToRedirect,
+  ]);
+
   if (modifyInterviewerMutationError) return <UserError />;
 
   const editInterviewer = () => {
-    if (newUser) {
+    if (isNewUser) {
       modifyInterviewer({
         variables: {
           information: {
@@ -44,8 +61,6 @@ const InterviewerEditProfile = () => {
         },
       });
     }
-    //TODO: Bug: Cannot redirect after first time
-    history.push(HOME_PATH);
   };
 
   return (
